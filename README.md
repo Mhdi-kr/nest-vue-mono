5de16bd145957497056c97855e6ce959223b2145
Mahdi Karimi <mhdi_kr@outlook.com>
2022-01-17 00:20:40 +0330

    feat: init ✨

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# nest-vue-mono
-Nest
+# Fullstack Application boilerplate
+
+## Tech Stack
+
+### Frontend
+
+The application uses vue 3 as the frontend framwork, vite build system, tailwind css as the css library
+
+### Backend
+
+The application uses Nestjs as its basckend framework
+
+### deployment
---
+++ packages/client/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="h-screen flex flex-col items-center justify-center">
+        <img class="mb-4" alt="Vue logo" src="./assets/logo.png" />
+        <HelloWorld msg="Client Application" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+onMounted(async (): Promise<void> => {
+    // This is called when the component is mounted
+    const res = await fetch("http://localhost:8090/");
+    const json = await res.json();
+    console.log(json);
+});
+</script>
