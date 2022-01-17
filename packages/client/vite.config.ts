dc44a052866414080b49278a0b03daa94d5f8d18
Mahdi Karimi <mhdi_kr@outlook.com>
2022-01-17 03:54:11 +0330

    feat: added PWA to client 📱

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,10 +1,67 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import Vue from "@vitejs/plugin-vue";
+import { ManifestOptions, VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+import replace from "@rollup/plugin-replace";
 
-// https://vitejs.dev/config/
+const pwaOptions: Partial<VitePWAOptions> = {
+    mode: "development",
+    base: "/",
+    includeAssets: ["favicon.svg"],
+    manifest: {
+        name: "PWA Router",
+        short_name: "PWA Router",
+        theme_color: "#ffffff",
+        icons: [
+            {
+                src: "pwa-192x192.png", // <== don't add slash, for testing
+                sizes: "192x192",
+                type: "image/png",
+            },
+            {
+                src: "/pwa-512x512.png", // <== don't remove slash, for testing
+                sizes: "512x512",
+                type: "image/png",
+            },
+            {
+                src: "pwa-512x512.png", // <== don't add slash, for testing
+                sizes: "512x512",
+                type: "image/png",
+                purpose: "any maskable",
+            },
+        ],
+    },
+    devOptions: {
+        enabled: process.env.SW_DEV === "true",
+        /* when using generateSW the PWA plugin will switch to classic */
+        type: "module",
+        navigateFallback: "index.html",
+    },
+};
+
+const replaceOptions = { __DATE__: new Date().toISOString() };
+const claims = process.env.CLAIMS === "true";
+const reload = process.env.RELOAD_SW === "true";
+
+if (process.env.SW === "true") {
+    pwaOptions.srcDir = "src";
+    pwaOptions.filename = claims ? "claims-sw.ts" : "prompt-sw.ts";
+    pwaOptions.strategies = "injectManifest";
+    (pwaOptions.manifest as Partial<ManifestOptions>).name =
+        "PWA Inject Manifest";
+    (pwaOptions.manifest as Partial<ManifestOptions>).short_name = "PWA Inject";
+}
+
+if (claims) pwaOptions.registerType = "autoUpdate";
+
+if (reload) {
+    // @ts-ignore
+    replaceOptions.__RELOAD_SW__ = "true";
+}
+
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 8080,
-  }
-})
+    // base: process.env.BASE_URL || 'https://github.com/',
+    build: {
+        sourcemap: process.env.SOURCE_MAP === "true",
+    },
+    plugins: [Vue(), VitePWA(pwaOptions), replace(replaceOptions)],
+});
